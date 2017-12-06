b3aa0311472ee6a7cfe1090b61792aee827ec5d1
Wouter van den Heuvel <exo_00@hotmail.com>
2017-12-06 12:08:06 +0100

    Initial commit

---
+++ Assets/GyroLook.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroLook : MonoBehaviour {
+
+	Quaternion offset;
+
+	void Awake()
+	{
+		Input.gyro.enabled = true;
+	}
+
+	void Start()
+	{
+		//Subtract Quaternion
+		offset = transform.rotation * Quaternion.Inverse(GyroToUnity(Input.gyro.attitude));
+	}
+
+	void Update()
+	{
+		GyroModifyCamera();
+	}
+
+	void GyroModifyCamera()
+	{
+		//Apply offset
+		transform.rotation = offset * GyroToUnity(Input.gyro.attitude);
+	}
+
+	private static Quaternion GyroToUnity(Quaternion q)
+	{
+		return new Quaternion(q.x, q.y, -q.z, -q.w);
+	}
+}
